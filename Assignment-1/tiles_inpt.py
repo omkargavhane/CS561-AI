3cc2dd93ea9d132c9a08ea77e7160c3cc3b63832
omkar gavhane <41783462+omkargavhane@users.noreply.github.com>
2021-09-06 22:24:26 +0530

    Add files via upload

---
+++ Assignment-1/tiles_inpt.py
@@ -0,0 +1,18 @@
+#input to A* , BFS(best first search) ,Simulated annealing ,hill climbing algo
+
+start_state=[[6,7,3],
+            [8,4,2],
+            [1,None,5]]
+goal_state=[[1,2,3],
+            [4,5,6],
+            [7,8,None]]
+'''
+
+start_state=[[1,2,3],
+             [4,None,6],
+             [7,5,8]]
+
+goal_state=[[1,2,3],
+            [4,5,6],
+            [7,8,None]]
+'''
